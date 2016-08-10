344fe8e3aecfddc716b949dd6a6341e638f47cd3
Koichi Nakashima <koichi@nksm.name>
2016-08-10 13:45:46 +0900

    rails-various: create order#create

--- rails-various/app/controllers/orders_controller.rb
+++ rails-various/app/controllers/orders_controller.rb
@@ -6,4 +6,14 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def create
+    @order = Order.create(order_params)
+    redirect_to @order
+  end
+
+  private
+    def order_params
+      params.require(:order).permit(:name, :address)
+    end
 end
--- rails-various/config/routes.rb
+++ rails-various/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  #resources :orders
   get 'orders' => 'orders#index'
-  get 'orders/:id', to: 'orders#show', as: 'order'
+  get 'orders/new' => 'orders#new', as: 'new_order'
+  get 'orders/:id' => 'orders#show', as: 'order'
+  post 'orders' => 'orders#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
