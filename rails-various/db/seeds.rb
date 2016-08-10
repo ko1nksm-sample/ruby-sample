e9062a564a7d51e5954b00fe092c9b5bf6eb6c26
Koichi Nakashima <koichi@nksm.name>
2016-08-10 16:33:07 +0900

    rails-various: Update seed.rb

--- rails-various/db/seeds.rb
+++ rails-various/db/seeds.rb
@@ -6,10 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Order.create(:name => 'name1', :address => 'address1', :details_attributes => [
-  {:product => 'product1', :quantity => 1},
-  {:product => 'product2', :quantity => 2},
-  {:product => 'product3', :quantity => 3},
-])
+order = Order.create(:name => 'name1', :address => 'address1')
+Detail.create(:order => order, :product => 'product1', :quantity => 1)
+Detail.create(:order => order, :product => 'product1', :quantity => 1)
+Detail.create(:order => order, :product => 'product1', :quantity => 1)
 Order.create(:name => 'name2', :address => 'address2')
 Order.create(:name => 'name3', :address => 'address3')
