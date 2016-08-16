fb0cd2bea035438d62d86dcb3979b71c4ddffca4
Koichi Nakashima <koichi@nksm.name>
2016-08-16 10:56:17 +0900

    rails-various: add validation

--- rails-various/app/models/detail.rb
+++ rails-various/app/models/detail.rb
@@ -1,3 +1,6 @@
 class Detail < ApplicationRecord
   belongs_to :order, required: false
+
+  validates :product, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 end
--- rails-various/app/models/order.rb
+++ rails-various/app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
    has_many :details, dependent: :destroy
    accepts_nested_attributes_for :details, allow_destroy: true
+
+  validates :name, presence: true
+  validates :address, presence: true
 end
