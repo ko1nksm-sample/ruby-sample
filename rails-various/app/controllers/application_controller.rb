3708e8718da274a46b4adb59a703aa95ef7dc6af
Koichi Nakashima <koichi@nksm.name>
2016-08-16 12:37:01 +0900

    rails-various: i18n

--- rails-various/app/controllers/application_controller.rb
+++ rails-various/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :set_locale
+
+  def set_locale
+    # if params[:locale] is nil then I18n.default_locale will be used
+    I18n.locale = params[:locale]
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
 end
 
--- rails-various/config/routes.rb
+++ rails-various/config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  #resources :orders
-  get 'orders' => 'orders#index'
-  get 'orders/new' => 'orders#new', as: 'new_order'
-  get 'orders/:id' => 'orders#show', as: 'order'
-  post 'orders' => 'orders#create'
-  delete 'orders/:id' => 'orders#destroy'
-  get 'orders/:id/edit' => 'orders#edit', as: 'edit_order'
-  patch 'orders/:id' => 'orders#update'
+
+  scope "(:locale)" do
+    #resources :orders
+    get 'orders' => 'orders#index'
+    get 'orders/new' => 'orders#new', as: 'new_order'
+    get 'orders/:id' => 'orders#show', as: 'order'
+    post 'orders' => 'orders#create'
+    delete 'orders/:id' => 'orders#destroy'
+    get 'orders/:id/edit' => 'orders#edit', as: 'edit_order'
+    patch 'orders/:id' => 'orders#update'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
